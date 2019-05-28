7ffb55147b599cd0ac2136a97e60c85b67f34f9b
Cristian221995 <crisfonteina@gmail.com>
2019-05-27 21:49:04 -0300

    Correccion de implemendacion de model mapper

--- src/main/java/com/example/SimulacroParcial/controller/TeamController.java
+++ src/main/java/com/example/SimulacroParcial/controller/TeamController.java
@@ -2,6 +2,7 @@ package com.example.SimulacroParcial.controller;
 
 import com.example.SimulacroParcial.model.Player;
 import com.example.SimulacroParcial.model.Team;
+import com.example.SimulacroParcial.model.TeamMapper;
 import com.example.SimulacroParcial.repository.TeamRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,6 @@ import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/team")
 @RestController
@@ -31,30 +31,30 @@ public class TeamController {
 
     @PostMapping("")
     public void addTeam(@RequestBody final Team t){
-        teamRepository.save(mm.map(t,Team.class));
+        teamRepository.save(t);
     }
 
     @GetMapping("")
-    public List<Team> getAll(){
-        List<Team> list = new ArrayList<>();
+    public List<TeamMapper> getAll(){
+        List<TeamMapper> list = new ArrayList<>();
 
         for (Team t: teamRepository.findAll()){
-            list.add(mm.map(t,Team.class));
+            list.add(mm.map(t,TeamMapper.class));
         }
         return list;
     }
 
     @PostMapping("/addPlayer/{idEquipo}")
     public void addPlayer(@PathVariable final Integer idEquipo, @RequestBody final Player p){
-        Team t = mm.map(teamRepository.findById(idEquipo).orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, String.format(TEAM_NOT_FOUND,idEquipo))),Team.class);
+        Team t = teamRepository.findById(idEquipo).orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, String.format(TEAM_NOT_FOUND,idEquipo)));
         t.getPlayers().add(p);
         p.setTeam(t);
         this.addTeam(t);
     }
 
     @GetMapping("/mapper/{idEquipo}")
-    public Optional getWithMapper(@PathVariable final Integer idEquipo){
-        return mm.map(teamRepository.findById(idEquipo),Optional.class);
+    public TeamMapper getWithMapper(@PathVariable final Integer idEquipo){
+        return mm.map(teamRepository.findById(idEquipo),TeamMapper.class);
     }
 
     /** Comentado por optimizacion de codigo
@@ -67,12 +67,6 @@ public class TeamController {
             list.add(mm.map(t,Team.class));
         }
         return list;
-    }
-
-    @PostMapping("/mapper/")
-    public void addTeamWithMapper(@RequestBody final Team t){
-        ModelMapper mm = new ModelMapper();
-        teamRepository.save(mm.map(t,Team.class));
     }
     **/
 }
--- src/main/java/com/example/SimulacroParcial/model/TeamMapper.java
+++ src/main/java/com/example/SimulacroParcial/model/TeamMapper.java
@@ -10,7 +10,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -18,17 +17,12 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-public class Team {
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class TeamMapper {
 
     @NotNull(message = "Name cannot be null")
     @Size(min = 5, max = 200, message = "Name must be between 10 and 200 characters")
     private String name;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "team")
-    @Size(max = 23)
+    @Size(max = 23, message = "Team must have a maximum of 23 players")
     private List<Player> players;
 }
